4306aa98a8418caa4aecb04f71f773ce5a1a13e1
Chitransh Prasanna <chitranshprasanna6666@gmail.com>
2024-07-30 13:30:38 +0530

    Setup opf smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = ()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.listen(PORT, ()=>{
-        console.log(`Server started at PORT : ${PORT}`)
+        console.log(`Server started at PORT : ${PORT}`);
+        sendBasicEmail(
+            'support@admin.com',
+            'chitranshprasanna@gmail.com',
+            'This is a testing email',
+            'Hey, how are you, I hope you like the support'
+        );
+
     });
 }
 setupAndStartServer();
